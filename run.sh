185430874d8441b4adeba900a31acc674e16aa0e
conor1984 <conor.nagle1984@gmail.com>
2014-12-30 23:49:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@
 #service postgresql stop >/dev/null 2>&1
 #sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf >/var/log/postgresql/logfile.log 2>&1 & 
 
-
 #change the password
 #service postgresql start >/dev/null 2>&1
 #if [ ! -f /.postgres_pass_modified ]; then
@@ -12,7 +11,6 @@
 #fi
 #service postgresql stop >/dev/null 2>&1
 
-exec  /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf 
-#postgres -D /usr/local/pgsql/data >logfile 2>&1 &
+#sudo -u postgres -c -D /var/lib/postgresql/9.4/main >logfile 2>&1 &
 
 
