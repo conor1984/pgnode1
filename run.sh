e44dfb22b5c08852e493f7627041d1d542fd236f
conor1984 <conor.nagle1984@gmail.com>
2014-12-30 21:21:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,14 @@
 #sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf >/var/log/postgresql/logfile.log 2>&1 & 
 
 
+#change the password
+service postgresql start >/dev/null 2>&1
+if [ ! -f /.postgres_pass_modified ]; then
+	/modify_postgres_pass.sh
+fi
+service postgresql stop >/dev/null 2>&1
 
-postgres -D /usr/local/pgsql/data >logfile 2>&1 &
+sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf 
+#postgres -D /usr/local/pgsql/data >logfile 2>&1 &
 
 
