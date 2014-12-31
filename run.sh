8a2dde133feb43d9eb1bde9b6ccca7ff3569b10c
conor1984 <conor.nagle1984@gmail.com>
2014-12-30 21:03:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf >/var/log/postgresql/logfile.log 2>&1 & 
 
-createdb test
+
 
 
 
