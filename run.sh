4beac783cbb146511c514b232940eec3d857efa8
conor1984 <conor.nagle1984@gmail.com>
2014-12-30 21:06:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-
+service postgresql stop >/dev/null 2>&1
 sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf >/var/log/postgresql/logfile.log 2>&1 & 
 
 
-/etc/init.d/postgresql start
+
 
 
 
